c1fb2490c2c589764e89fdd7306efd5f9bd5b092
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-10-23 14:47:45 +0200

    Delete redmine_due_date_reminder directory

--- redmine_due_date_reminder/app/models/due_date_reminder_mailer.rb
+++
@@ -1,9 +0,0 @@
-# plugins/redmine_due_date_reminder/app/models/due_date_reminder_mailer.rb
-class DueDateReminderMailer < Mailer
-    def reminder(issue, days_left)
-      @issue = issue
-      @days_left = days_left
-      mail to: issue.assigned_to.mail, subject: "Reminder: Issue ##{issue.id} is due in #{@days_left} days"
-    end
-  end
-  
--- redmine_due_date_reminder/config/routes.rb
+++
@@ -1,5 +0,0 @@
-# plugins/redmine_due_date_reminder/config/routes.rb
-RedmineApp::Application.routes.draw do
-    # No routes needed for this simple plugin
-  end
-  
