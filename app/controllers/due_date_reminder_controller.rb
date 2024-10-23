ac3542e2a296ce24ec991b0e501d8ea1fdaec627
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-10-23 15:35:42 +0200

    Add files via upload

---
+++ app/controllers/due_date_reminder_controller.rb
@@ -0,0 +1,17 @@
+class DueDateReminderController < ApplicationController
+    before_action :find_issue, only: [:send_reminder]
+    before_action :authorize, only: [:send_reminder]
+  
+    def send_reminder
+      DueDateReminderMailer.send_reminder(@issue).deliver_now
+      flash[:notice] = l(:notice_due_date_reminder_sent)
+      redirect_to issue_path(@issue)
+    end
+  
+    private
+  
+    def find_issue
+      @issue = Issue.find(params[:id])
+    end
+  end
+  
