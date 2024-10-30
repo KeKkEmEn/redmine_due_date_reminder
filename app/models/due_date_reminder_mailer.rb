d6ba539eb8b34f949a5eb088c7008c451f0a763c
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-10-30 11:19:42 +0100

    Update due_date_reminder_mailer.rb

--- app/models/due_date_reminder_mailer.rb
+++ app/models/due_date_reminder_mailer.rb
@@ -1,6 +1,5 @@
+# plugins/redmine_due_date_reminder/app/models/due_date_reminder_mailer.rb
 class DueDateReminderMailer < Mailer
-  def self.issue_edit(recipient_user, issue, journal = nil)
-    
   def send_reminder(issue)
     recipients = issue.assigned_to.try(:mail)
     mail(to: recipients, subject: "Reminder: Issue ##{issue.id} is due soon") do |format|
