4b6b03de8caaf1d03de4a65eca3d0c378ef5d0aa
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-10-30 10:37:14 +0100

    Update due_date_reminder_controller.rb

--- app/controllers/due_date_reminder_controller.rb
+++ app/controllers/due_date_reminder_controller.rb
@@ -3,25 +3,18 @@ class DueDateReminderController < ApplicationController
  before_action :authorize, only: [:send_reminder]
  skip_before_action :authorize, only: [:send_reminder]
     
-    def send_reminder
-      DueDateReminderMailer.send_reminder(@issue).deliver_now
-      flash[:notice] = l(:notice_due_date_reminder_sent)
-      redirect_to issue_path(@issue)
-      @issue = Issue.find(params[:id])
+def send_reminder
+  @issue = Issue.find(params[:id])
+  recipient_user = @issue.assigned_to || @issue.author
 
-      # Try to get the assigned user; fallback to the author if none is assigned
-      recipient_user = @issue.assigned_to || @issue.author
-
-      # Check if recipient_user is available
-      if recipient_user
-        # Send the reminder email
-        Mailer.deliver_due_date_reminder(recipient_user, @issue)
-        redirect_to @issue, notice: 'Reminder sent successfully!'
-      else
-        # Show an error if no recipient is found
-        redirect_to @issue, alert: 'No recipient found for this reminder. Please assign the issue to a user first.'
-      end
-    end
+  if recipient_user
+    # Use a standard email method if deliver_due_date_reminder is causing issues
+    Mailer.issue_edit(recipient_user, @issue.journals.last, @issue).deliver_now
+    redirect_to @issue, notice: 'Reminder sent successfully!'
+  else
+    redirect_to @issue, alert: 'No recipient found for this reminder. Please assign the issue to a user first.'
+  end
+end
     private
   
     def find_issue
