644481d9fe8723502ff6beb0684656a83fbbb50f
KeKkEmEn <46707868+KeKkEmEn@users.noreply.github.com>
2024-10-30 11:05:23 +0100

    Update mailer.rb

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,6 +1,6 @@
 class DueDateReminderMailer < Mailer
   def self.issue_edit(recipient_user, issue, journal = nil)
-end
+    
   def send_reminder(issue)
     recipients = issue.assigned_to.try(:mail)
     mail(to: recipients, subject: "Reminder: Issue ##{issue.id} is due soon") do |format|
